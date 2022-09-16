20e6aaade0470122b4aa66672fb70da5126ddd9f
José Manuel Bernabé <josemanuel.bernabe@gmail.com>
2022-09-16 16:38:52 +0200

    NLP mejorado en DataMining

--- module_cognitive_treelogic/DataMining.py
+++ module_cognitive_treelogic/DataMining.py
@@ -1,23 +1,32 @@
+
 from SPARQLWrapper import SPARQLWrapper, JSON
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from nltk.corpus import stopwords
+from gensim.models import Word2Vec
+from umap import UMAP
+import copy
 import json
 import os
 import pandas as pd
 import numpy as np
-from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import re
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from umap import UMAP
 import hdbscan
-from sklearn.preprocessing import LabelEncoder
+import nltk
+nltk.download('stopwords')
+
 
 
 le_category = LabelEncoder()
 le_email = LabelEncoder()
 le_tag = LabelEncoder()
 dataframe = None
+
 
 def __load_data():
     sparql = SPARQLWrapper("http://edma.gnoss.com:8890/sparql",)
@@ -49,8 +58,7 @@ def __load_data():
     ?nodo <http://www.w3.org/2008/05/skos#prefLabel> ?nombrecategoria.
     }order by desc(?num)
     """
-    #Extraeremos el area tematica con la API de esta forma podremos relacionarlos con las areas tematicas que hay 
-    #en la odontologia, ya que son distintas a las del SGI.
+    
     try:
         results = ""
         results = sparql.query().convert()
@@ -66,32 +74,63 @@ def __load_data():
     df_processing = df_processing.drop('person',1)
     df_processing = df_processing.drop('nombrepersona',1)
     df_processing = df_processing.drop('num',1)
-    #print(df_processing.head())
-    df_processing.nombrecategoria = le_category.fit_transform(df_processing.nombrecategoria) #Vectorizamos las palabras
-    df_processing.email = le_email.fit_transform(df_processing.email) #Vectorizamos las palabras
-    df_processing.tag = le_tag.fit_transform(df_processing.tag) #Vectorizamos las palabras
-    return df_processing
+    
+    #Limpiamos los tags y quitamos los stopwords
+    df_processing['tag'] = df_processing['tag'].str.lower()
+    df_processing['tag'] = df_processing['tag'].str.replace('[^a-zA-Z]',' ',regex=True)
+    df_processing['tag'] = df_processing['tag'].apply(lambda x: ' '.join([word for word in x.split() if word not in stopwords.words('english') and word not in stopwords.words('spanish')]))
+    df_processing['nombrecategoria'] = df_processing['nombrecategoria'].str.lower()
+    df_processing['nombrecategoria'] = df_processing['nombrecategoria'].str.replace('[^a-zA-Z]',' ',regex=True)
+    df_processing['nombrecategoria'] = df_processing['nombrecategoria'].apply(lambda x: ' '.join([word for word in x.split() if word not in stopwords.words('english') and word not in stopwords.words('spanish')]))
+    
+    #Combinamos Categoria y Tags para mayor vocabulario
+    df_processing['combine'] = df_processing.values.tolist()
+    df_processing['combine'] = df_processing[['tag','nombrecategoria']].values.tolist()
+    dictionary_nlp = df_processing['combine'].tolist()
+    #Generamos el vocabulario y vectorizamos las palabras (NLP)
+    model = Word2Vec(dictionary_nlp, min_count=3)
+    
+    #print(model.wv['sdn'])
+    return df_processing, model
 
 def load_model():
     '''Load model UMAP nad HDB CLUSTER'''
-    global dataframe
-    df_processing = __load_data()
-    #Cargamos la libreria
-    u = UMAP()
-    projection = u.fit_transform(df_processing)
+    global dataframe, model
+    df_processing, model = __load_data()
+    #Volcamos la informacion del modelo para pasarle UMAP
+    data_dict = []
+    for i in range(0, len(model.wv)): 
+        data_dict.append(model.wv[model.wv.index_to_key[i]])
+
+    #Cargamos la libreria UMAP (machine learning)
+    u = UMAP(n_neighbors=15, target_metric= 11)
+    projection = u.fit_transform(data_dict)
+    
+    #Añadimos al dataframe los vectores generados a traves de las palabras
+    df_processing['vector_word'] = df_processing['tag'].apply(lambda x: projection[model.wv.key_to_index[x]] if x in model.wv else None) #En vez de coger los valores del model, cogemos los valores de UMAP, ya que ha reducido la dimensión
+    df_processing['similarity'] = df_processing['tag'].apply(lambda x: model.wv.most_similar(x) if x in model.wv else None)
+    #Eliminamos las filas donde hay nulos
+    df_processing = df_processing.dropna(axis=0)
+    df_processing['similarity'] = df_processing['similarity'].apply(lambda x: [ projection[model.wv.key_to_index[word[0]]] for word in x])
+
+    
+    df_process_cluster = pd.DataFrame()
+    df_process_cluster[['vector_word_1','vector_word_2']] = pd.DataFrame(df_processing.vector_word.tolist(), index= df_processing.index)
+    np_similarity = np.array(df_processing.similarity.tolist())
+    np_similarity = np_similarity.reshape((len(df_processing), 10*np_similarity.shape[2]))
+    df_process_cluster = pd.concat([df_process_cluster, pd.DataFrame(np_similarity)], axis=1, join='inner')
+    #Generamos los clusters
     clusterer = hdbscan.HDBSCAN(min_cluster_size=15, gen_min_span_tree=True)
-    clusterer.fit(projection)
+    clusterer.fit(df_process_cluster)
+    
     labels = clusterer.labels_
-    hdb_cluster = pd.DataFrame(labels)  
-    df_final = pd.concat([df_processing, hdb_cluster], axis=1)
-    df_final.columns = ['tag','nombrecategoria', 'email', 'hdb_cluster']
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    #print('Estimated number of clusters: %d' % n_clusters_)
-    dataframe = df_final
-    #Revertimos la vectorizacion
-    dataframe.email = le_email.inverse_transform(dataframe.email)
-    dataframe.nombrecategoria = le_category.inverse_transform(dataframe.nombrecategoria)
-    dataframe.tag = le_tag.inverse_transform(dataframe.tag)
+    hdb_cluster = pd.DataFrame(labels)
+    # n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    # print('Estimated number of clusters: %d' % n_clusters_)
+    df_cluster = pd.concat([df_processing, hdb_cluster], axis=1, join='inner')
+    df_cluster.columns = ['tag','nombrecategoria', 'email', 'combine','vector_word', 'similarity', 'hdb_cluster']
+    dataframe = df_cluster
+    
 
 def get_all_cluster():
     '''Return dataframe generated with clustering'''
@@ -158,11 +197,3 @@ def get_research_category_tag(tags: list, categories: list):
     df_categories = dataframe[dataframe['nombrecategoria'].isin(categories)]
     df = df_tags.merge(df_categories, how='inner',on='email')
     return list(set(df['email'].to_list()))
-
-"""
-load_model()
-print(get_all_tag())
-print(get_research_tag(['Internet of Things','IoT','security']))
-print(get_research_category(['Physical Sciences','Computer Science']))
-print(get_research_category_tag(['Internet of Things','IoT','security'],['Physical Sciences','Computer Science']))
-"""
